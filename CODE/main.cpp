0576cb731f3bcce85a0c4d08f31e18c084d59482
Hagen <hagensays@gmail.com>
2026-08-12 19:13:12 +0200

    Add Hagenware v0.1.0 application

---
+++ CODE/main.cpp
@@ -0,0 +1,68 @@
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+
+namespace {
+constexpr wchar_t kClassName[] = L"HagenwareWindow";
+constexpr wchar_t kWindowTitle[] = L"Hagenware";
+constexpr wchar_t kMessage[] = L"Hagenware v0.1.0";
+
+LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam) {
+    switch (message) {
+    case WM_PAINT: {
+        PAINTSTRUCT paint{};
+        HDC dc = BeginPaint(window, &paint);
+        RECT client{};
+        GetClientRect(window, &client);
+        DrawTextW(dc, kMessage, -1, &client, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        EndPaint(window, &paint);
+        return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProcW(window, message, wparam, lparam);
+    }
+}
+} // namespace
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int show_command) {
+    WNDCLASSW window_class{};
+    window_class.lpfnWndProc = WindowProc;
+    window_class.hInstance = instance;
+    window_class.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+    window_class.lpszClassName = kClassName;
+
+    if (RegisterClassW(&window_class) == 0) {
+        return 1;
+    }
+
+    HWND window = CreateWindowExW(
+        0,
+        kClassName,
+        kWindowTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        720,
+        480,
+        nullptr,
+        nullptr,
+        instance,
+        nullptr);
+
+    if (window == nullptr) {
+        return 1;
+    }
+
+    ShowWindow(window, show_command);
+
+    MSG message{};
+    while (GetMessageW(&message, nullptr, 0, 0) > 0) {
+        TranslateMessage(&message);
+        DispatchMessageW(&message);
+    }
+
+    return static_cast<int>(message.wParam);
+}

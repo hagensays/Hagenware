e1a21ab42c66e7d77d6549537fdbeb0b37b7f883
Hagen <hagensays@gmail.com>
2026-08-13 15:28:57 +0200

    Bump version to v0.4.11

--- CODE/version.h
+++ CODE/version.h
@@ -4,8 +4,8 @@ namespace Version {
 
 inline constexpr unsigned int kMajor = 0;
 inline constexpr unsigned int kMinor = 4;
-inline constexpr unsigned int kPatch = 10;
+inline constexpr unsigned int kPatch = 11;
 inline constexpr unsigned int kPacked = (kMajor * 1000000u) + (kMinor * 1000u) + kPatch;
-inline constexpr wchar_t kNumber[] = L"v0.4.10";
+inline constexpr wchar_t kNumber[] = L"v0.4.11";
 
 } // namespace Version

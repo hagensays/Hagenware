c4d867fb974cbcaa18da69dcb5d7d5f1fca5e1cb
Hagen <hagensays@gmail.com>
2026-08-13 16:42:36 +0200

    Add Scanner path and export setup controls

--- CODE/version.h
+++ CODE/version.h
@@ -4,8 +4,8 @@ namespace Version {
 
 inline constexpr unsigned int kMajor = 0;
 inline constexpr unsigned int kMinor = 4;
-inline constexpr unsigned int kPatch = 13;
+inline constexpr unsigned int kPatch = 14;
 inline constexpr unsigned int kPacked = (kMajor * 1000000u) + (kMinor * 1000u) + kPatch;
-inline constexpr wchar_t kNumber[] = L"v0.4.13";
+inline constexpr wchar_t kNumber[] = L"v0.4.14";
 
 } // namespace Version
